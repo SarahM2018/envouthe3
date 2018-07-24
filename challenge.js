7db79fa50e2d1465e5759570b5f08bc47c354d15
LeaMehilli <40993603+LeaMehilli@users.noreply.github.com>
2018-07-24 10:22:23 +0200

    sitedeouf

---
+++ challenge.js
@@ -0,0 +1,12 @@
+var bullets = 1
+var swords = 2
+var crochet = 1
+if (bullets == 3 & swords == 2 & crochet == 1){
+  alert("attack");
+}else{
+  alert("pas d'attack");
+}
+var membres = ["barbe noire", "crochet", "jack sparrow", "le golif"];
+for (compteur = 0; compteur < membres.length; compteur++){
+  alert(membres[compteur]);
+}
