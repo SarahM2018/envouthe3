18ab9663e2299f471fa5fd1e32334ae98bbc6104
LeaMehilli <40993603+LeaMehilli@users.noreply.github.com>
2018-07-26 11:50:55 +0200

    commit

--- challenge.js
+++ challenge.js
@@ -1,12 +1,17 @@
-var bullets = 1
-var swords = 2
-var crochet = 1
-if (bullets == 3 & swords == 2 & crochet == 1){
-  alert("attack");
-}else{
-  alert("pas d'attack");
+$(document).keyup(e => {
+  if (e.keyCode == 27) {
+    $("body").css("background-color", "pink");
 }
-var membres = ["barbe noire", "crochet", "jack sparrow", "le golif"];
-for (compteur = 0; compteur < membres.length; compteur++){
-  alert(membres[compteur]);
+})
+
+$(document).keyup(e => {
+  if (e.keyCode == 38) {
+$("body").css("background-color", "yellow");
 }
+})
+
+$(document).keyup(e => {
+  if (e.keyCode == 40) {
+$("body").css("background-color", "blue");
+}
+})
